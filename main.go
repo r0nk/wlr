95f6861757b82e1b5b3b10e3374d286ea2f4918b
r0nk <r0nk@simplecpu.com>
2023-10-19 23:51:47 -0500

    MULTI WORD LIST SUPPORT CAPS LOCK BECAUSE I DONT NEED SLEEP

--- main.go
+++ main.go
@@ -9,61 +9,77 @@ import (
 )
 
 func usage() {
-	fmt.Println("Usage: wlr [<input>] [-file=<filename>]")
+	fmt.Println("Usage: wlr <input> [filename]")
 	fmt.Println("Examples:")
 	fmt.Println("cat wordlist.txt | wlr \"replace: FUZZ\" ")
 }
 
 func main() {
-	var input string
 	var file string
 
 	flag.StringVar(&file, "file", "", "File containing replacement strings")
+	//TODO attack modes
+	//clusterbomb
+	//pitchfork
+	//sniper
 
 	flag.Parse()
 
-	// Get non-flag arguments
 	args := flag.Args()
 
-	if len(args) > 0 {
-		input = args[0]
-	} else {
+	if len(args) <= 0 {
 		usage()
 		return
 	}
 
-	var lines []string
+	input := args[0]
 
-	// Read lines from file or stdin
-	if file != "" {
+	for i, str := range args {
+		if i == 0 { // skip the string to replace
+			if len(args) == 1 {
+				str = "" //read from stdin instead
+			} else {
+				continue
+			}
+		}
+		parts := strings.Split(str, ":")
+		file := parts[0]
+		placeholder := "FUZZ"
+		if len(parts) == 2 {
+			placeholder = parts[1]
+		}
+		var lines []string
 		var err error
 		lines, err = readLines(file)
 		if err != nil {
 			fmt.Println("Error reading file:", err)
 			return
 		}
-	} else {
+
+		for _, line := range lines {
+			modified := strings.Replace(input, placeholder, line, -1)
+			//sniper
+			fmt.Println(modified)
+		}
+	}
+
+}
+
+func readLines(filename string) ([]string, error) {
+	var lines []string
+	if filename == "" {
 		scanner := bufio.NewScanner(os.Stdin)
 		for scanner.Scan() {
 			lines = append(lines, scanner.Text())
 		}
+		return lines, scanner.Err()
 	}
-
-	// Replace 'FUZZ' and output the modified strings
-	for _, line := range lines {
-		modifiedString := strings.Replace(input, "FUZZ", line, -1)
-		fmt.Println(modifiedString)
-	}
-}
-
-func readLines(filename string) ([]string, error) {
 	file, err := os.Open(filename)
 	if err != nil {
 		return nil, err
 	}
 	defer file.Close()
 
-	var lines []string
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		lines = append(lines, scanner.Text())
